f65e62630a60a2a30faceac4a7f417b21576605d
Sepand <Sepand@SEPAND>
2023-11-01 11:21:46 +0330

    Add project files.

---
+++ average/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("how many points?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] num = new int[n];
+            float sum = 0, unit, unit1 = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write("pls enter number{0};", i + 1);
+                num[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("pls enter unit{0};", i + 1);
+                unit = Convert.ToInt32(Console.ReadLine());
+                sum += unit * num[i];
+                unit1 += unit;
+
+            }
+            float avg = sum / unit1;
+            Console.WriteLine("average you is:{0}", avg);
+            Console.ReadKey();
+        }
+    }
+}
+//سورنا کریمی
+//تمرین3
